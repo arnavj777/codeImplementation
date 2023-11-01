811886c22fd92b95eec9400a8c78a3c515f45cab
s462022 <91020017+s462022@users.noreply.github.com>
2023-11-01 14:09:58 -0500

    Update Runner.java

--- src/codeImplementation/Runner.java
+++ src/codeImplementation/Runner.java
@@ -1,6 +1,67 @@
 package codeImplementation;
 
-public class Runner {
-	
+public class Runner {	
+		// Initialize variables for the class itself
+		String name;
+		String userName;
+		String password;
+		double[] location = new double[2];
+		Boolean hasWatch;
+		double weight;
+		
+		// Construct the Runner object
+		public Runner(String name, String userName, String password,
+	double[] location, Boolean hasWatch, double weight)
+		{
+			this.name = name;
+			this.userName = userName;
+			this.password = password;
+			this.hasWatch = hasWatch;
+			this.weight = weight;
+
+			if(hasWatch)
+			{
+				this.location = location;
+			}
+			else
+			{
+				this.location[0] = 0.0;
+				this.location[1] = 0.0;
+			}
+		}
+
+		public void login(String userName, String password)
+		{
+			if(userName == this.userName)
+			{	
+				boolean loggedIn = false;
+				int count = 0;
+				while(!loggedIn) {
+					if(password.equals(this.password))
+					{
+						System.out.println("Logged in");
+					}
+					else
+					{
+						System.out.println("Password in correct");
+						count++;
+						if(count > 3)
+							{
+								System.out.println("You've been locked out for 10 minutes");
+								//Lock out user for 10 minutes
+							}
+					}
+				}
+			}
+			else
+			{
+				System.out.println("This username does not exist");
+			}
+		
+		}
+		
+		public static void Main(String[] args) {
+			
+		}
 	
 }
