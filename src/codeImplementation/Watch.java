6a4e6411e368828081fd1fd37b500e194cd9c8bd
s462022 <91020017+s462022@users.noreply.github.com>
2023-11-03 13:39:11 -0500

    Create Watch.java

---
+++ src/codeImplementation/Watch.java
@@ -0,0 +1,11 @@
+package codeImplementation;
+
+import java.util.ArrayList;
+
+public class Watch {
+	ArrayList<Double> times;
+	ArrayList<Double> miles;
+	ArrayList<Double> speed;
+
+	//watch to be implemented at a later date, all runners constructed should have no watch
+}
