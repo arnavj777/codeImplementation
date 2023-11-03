5cef50d060c903534ba5226474620953133f2e61
arnavj777 <64499454+arnavj777@users.noreply.github.com>
2023-11-03 14:16:43 -0500

    Update Calendar.java

--- src/codeImplementation/Calendar.java
+++ src/codeImplementation/Calendar.java
@@ -1,6 +1,8 @@
 package codeImplementation;
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import javax.swing.*;
@@ -11,13 +13,43 @@ public class Calendar {
 	String Weather;
 	String runningPath;
 	
-	Map<Date, Stats> Stats = new HashMap<Date, Stats>(){
-		put("")
-	};
+	// set up stats examples
 	
 	
+	Map<Date,Stats> Stats = new HashMap<Date, Stats>();
+	Map<LocalDateTime, String> notes = new HashMap<LocalDateTime, String>();
+		
+	
+    Iterator StatsIterator = Stats.entrySet().iterator();
 
 	
- }
+	
+	// calcAvgMileGoal() ----> calcAvgMileRan
+	public double calcAvgMileRan(LocalDateTime initalDate, LocalDateTime finalDate) {
+		double y = 0;
+		boolean x = true;
+		boolean z = false;
+		while (StatsIterator.hasNext() && x ) {
+			 
+	        Map.Entry mapElement
+	            = (Map.Entry)StatsIterator.next();
+	        
+	        if(mapElement.equals( initalDate)) {
+	        	z = true;
+	        }
+	        if(z) {
+	        y = y + (int)mapElement.getValue();
+	        }
+	        if()
+	        
+	    }
+		return y;
+		
+	}
+	
+	
+	
+ 
+}}
 
 
